9911153c71a1359065428c771c7ed09b50039e52
Oluwatobi lemon <joshval.lj@gmail.com>
2019-05-15 13:05:14 -0700

    Worked on the prime

--- Numbers.java
+++ Numbers.java
@@ -4,5 +4,28 @@
 import java.util.Scanner;
 
 public class Numbers {
-    
-}
+    public static void main(String[] args) {
+
+        int temp;
+        boolean isPrime = true;
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter Number: ");
+
+        int num = input.nextInt(); //Capture the input in an integer
+        input.close();
+
+            for (int x=2; x<=num/2; x++) {
+                 temp = num%x;
+            
+                if (temp==0){ 
+                isPrime=false;
+                 break;
+            }
+        }
+        if (isPrime && num != 0 && num != 1)
+            System.out.print(num + " is a Prime Number");
+          else
+            System.out.print(num + " is not a Prime Number");
+
+    }
+}    
