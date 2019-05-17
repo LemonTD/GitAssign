09a5e49423d66a6bf5cfa6029cb829a9e15158bd
Oluwatobi lemon <joshval.lj@gmail.com>
2019-05-17 09:32:53 -0700

    Changed few things

--- Blink.java
+++ Blink.java
@@ -7,18 +7,18 @@ import java.util.Scanner;
 public class Blink {
     public static void main(String[] args) {
         String reverseString = " ";
-        System.out.println("Enter String to reversed"); // Allow user to iput
+        System.out.println("Enter Word to reversed"); // Allow user to iput
 
-        Scanner scanner = new Scanner(System.in);
-        String input = scanner.nextLine();
+        Scanner scan = new Scanner(System.in);
+        String input = scan.nextLine();
 
         StringBuilder inputStr = new StringBuilder(input); //pass input to constructor of StringBuilder
 
         reverseString = inputStr.reverse().toString(); // Use reverse method to reserve the String
 
-        System.out.println("Initial String: " + input);
+        System.out.println("Initial Word: " + input);
         
-        System.out.println("Reversed String: " + reverseString);
+        System.out.println("Reversed Word: " + reverseString);
     }
 
 }
